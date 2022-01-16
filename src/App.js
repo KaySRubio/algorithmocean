85a37c6325afc3cbce961826aa8fafa32786a696
Kay Rubio <krubio@cainc.com>
2022-01-16 09:50:48 -0500

    Added functionality where you can now enter a search term to filter the list of algorithms

--- src/App.js
+++ src/App.js
@@ -68,7 +68,7 @@ const searchedAccounts = accounts.filter.(function (account) {
 });*/
 
   // dynamic value for searchTerm; if it updates, it will re-render the search component
-  const [searchTerm, setSearchTerm] = React.useState('Default');
+  const [searchTerm, setSearchTerm] = React.useState('');
 
   return (
     <div>
@@ -77,32 +77,44 @@ const searchedAccounts = accounts.filter.(function (account) {
       </nav>
 
       {/* Create Account section */}
-      <h1>Create an Account</h1>
       <label>
       </label>
       <h2>Welcome {account.name}</h2>
       <h2>Select an algorithm to practice</h2>
-      <ListAlg list={algorithms}/> {/* monkey; new component I'm making */}
 
-    <Search onSearch={handleSearch}/> {/* Render the search component. Pass it the property onSearch which is the handleSearch function */}
+      <Search onSearch={handleSearch}/> {/* Render the search component. Pass it the property onSearch which is the handleSearch function */}
+
+      <ListAlg list={algorithms} searchBy={searchTerm} /> {/* monkey; new component I'm making */}
 
     <ListStudentAccounts accounts={accounts} /> {/* Render the ListStudentAccounts component. Pass it the property accounts which is an array of account objects*/}
 
+
+   
     </div>
   );
 }
 
-const ListAlg = ({ list }) => {
+const ListAlg = ({ list, searchBy }) => {
+
+  const searchedList = list.filter(function (item) {
+    return item.toLowerCase().includes(searchBy.toLowerCase());
+  });
 
   return (
     <div>
-      <p>Pick an algorithm to practice</p>
+ 
       <ul>
 
+      {/* List without search option 
       {list.map((item, index) => (
           <li key={index}><a href={item}>{item}</a></li>
           ))}
-        
+        */}
+
+
+      {searchedList.map((item, index) => (
+          <li key={index}><a href={item}>{item}</a></li>
+          ))} 
       </ul>
     </div>
   )
@@ -139,7 +151,6 @@ const Search = ({ onSearch }) => {
   // in JSX, when data in the input is changed, run handleChange function, also show the searchTerm
   return ( 
     <div>
-      <h1>Search</h1>
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" onChange={onSearch}/>
       {/*<p> Searching for <strong>{searchTerm}</strong>.</p>*/}
