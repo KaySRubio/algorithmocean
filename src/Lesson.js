5f00da0330e2456b583271eba568d59ffccc5f07
Kay Rubio <krubio@cainc.com>
2022-04-08 14:22:03 -0400

    Creating array of random numbers and using that to populate textSquares in canvas

--- src/Lesson.js
+++ src/Lesson.js
@@ -3,37 +3,43 @@ import { Stage, Shape, Container, Text, Ticker } from '@createjs/easeljs';
 import Navbar from './Navbar';
 import Toolbox from './Toolbox';
 
-
 class Lesson extends React.Component {
   constructor(props) {
     super(props);
     this.state = { };
-}
-
+    this.numbers = []; // array for the numbers that will be sorted
+  }
+  
   componentDidMount = () => {
+    this.initializeArray();
     this.init();
   }
 
-  init() {
+  // Initialize an array of 6 elements with random numbers [10-100]
+  initializeArray = () => {
+    this.numbers[0] = Math.floor(Math.random()*100);
+    // Set the rest of the numbers ensuring no repeats
+    for (let i = 1; i < 6; i++ ) {
+      let m = Math.floor(Math.random()*100);
+      while(this.numbers.includes(m) && m < 10){
+          m = Math.floor(Math.random()*100);
+      }
+      m = Math.floor(Math.random()*100);
+      this.numbers[i] = m;
+    }
+    console.log('array is ', this.numbers);
+  }
 
-    let xposition = 10;
-    let yposition = 10;
-    let number = 25;
+  // todo program another method to re-call initializeArray if the list is too close to sorted
+
+
+  // Initialize the createJS canvas
+  init() {
+    let x = 10;
+    let y = 10;
     let stage = new Stage('demoCanvas');
 
-    // Original code to add a shape and text inside a container 
-    let button1 = new Container();
-    let square = new Shape();
-    square.graphics.setStrokeStyle(3).beginStroke("white").beginFill("black").drawRect(xposition, yposition, 34, 25);
 
-    let text = new Text(number, '20px Arial', 'white');
-    text.textBaseline = "alphabetic";
-    text.x = xposition+6;
-    text.y = yposition+19;
-
-    button1.addChild(square, text);
-    stage.addChild(button1);
-    
     /* Move the whole button around
     //Update stage will render next frame
     Ticker.addEventListener("tick", handleTick);
@@ -46,26 +52,28 @@ class Lesson extends React.Component {
       stage.update();
     } */
     
-
-    let button2 = this.makeButton();
-    stage.addChild(button2);
-
+    for (let i = 0; i < this.numbers.length; i++ ) {
+        let textSquare1 = this.makeTextSquare(x, y, this.numbers[i]);
+        stage.addChild(textSquare1);
+        x += 40;
+    }
 
     stage.update(); 
     
-
   }
 
-  makeButton(){
-    let button1 = new Container();
+  makeTextSquare(x, y, num){
+    let textSquare = new Container();
     let square = new Shape();
-    square.graphics.setStrokeStyle(3).beginStroke("white").beginFill("black").drawRect(30, 30, 34, 25);
+    square.graphics.setStrokeStyle(3).beginStroke("white").beginFill("black").drawRect(x, y, 34, 25);
 
-    // let text = new Text(number, '20px Arial', 'white');
-    // text.textBaseline = "alphabetic";
+    let text = new Text(num, '20px Arial', 'white');
+    text.textBaseline = "alphabetic";
+    text.x = x+6;
+    text.y = y+19;
 
-    button1.addChild(square);
-    return button1;
+    textSquare.addChild(square, text);
+    return textSquare;
   }
 
   render(){
