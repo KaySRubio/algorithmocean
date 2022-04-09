41906ce56f482c615d3388897e568f5635b025f4
Kay Rubio <krubio@cainc.com>
2022-04-09 10:37:08 -0400

    Finished programming the swap animation

--- src/Lesson.js
+++ src/Lesson.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Stage, Shape, Container, Text, Ticker } from '@createjs/easeljs';
 import Navbar from './Navbar';
 import Toolbox from './Toolbox';
-
+import { Tween, Ease } from "@createjs/tweenjs";
 
 
 class Lesson extends React.Component {
@@ -12,6 +12,37 @@ class Lesson extends React.Component {
     this.numbers = []; // array for the numbers that will be sorted
     // this.targetClicks = 0;
   }
+
+  swap(){
+    let distance = this.operandLocationX[0] - this.operandLocationX[1];
+    let pow = 4;
+
+     /*This works OK, makes kind of a L-shape movement
+    Tween.get(this.operandContainers[0])
+      .to({ y: 40, x: this.operandContainers[0].x+distance*-1 }, 500, Ease.getPowIn(3))
+      .to({ y: 0 }, 500, Ease.getPowOut(3)); 
+    Tween.get(this.operandContainers[1])
+      .to({ y: 40, x: this.operandContainers[1].x+distance }, 500, Ease.getPowIn(3))
+      .to({ y: 0 }, 500, Ease.getPowIn(3));
+    */
+    //This works OK, makes kind of a V-shape movement
+    Tween.get(this.operandContainers[0])
+      .to({ y: 40, x: this.operandContainers[0].x+distance/2*-1 }, 500, Ease.getPowIn(pow))
+      .to({ y: 0, x: this.operandContainers[0].x+distance*-1 }, 500, Ease.getPowOut(pow)); 
+    Tween.get(this.operandContainers[1])
+      .to({ y: 80, x: this.operandContainers[1].x+distance/2 }, 500, Ease.getPowIn(pow))
+      .to({ y: 0, x: this.operandContainers[1].x+distance }, 500, Ease.getPowOut(pow));
+
+    // Tween.get(this.operandContainers[0]).to({ y: 100 }, 500, Ease.linear);
+    // Tween.get(this.operandContainers[0]).to({ y: 100 }, 500, Ease.bounceInOut(speed));
+
+    // Ticker.setFPS(60); // does not work?
+    Ticker.addEventListener("tick", this.stage);
+
+    // todo - remove event listener at some point after animations are done?
+
+  }
+
   
   componentDidMount = () => {
     this.initializeArray();
@@ -20,9 +51,13 @@ class Lesson extends React.Component {
 
   // Global variables
   canvasClicks = 0;
-  operation = 'swap';
+  operation = 'Swap';
   operands = [];
-  operandLocations = [];
+  operandLocationX = [];
+  // operandLocations = [];
+  operandContainers = [];
+  stage = {};
+
 
   // Initialize an array of 6 elements with random numbers [10-100]
   initializeArray = () => {
@@ -52,8 +87,8 @@ class Lesson extends React.Component {
   init() {
     let x = 10;
     let y = 10;
-    let stage = new Stage('demoCanvas');
-
+    // let stage = new Stage('demoCanvas');
+    this.stage = new Stage('demoCanvas');
 
     /* Move the whole button around
     //Update stage will render next frame
@@ -71,50 +106,63 @@ class Lesson extends React.Component {
 
     for (let i = 0; i < this.numbers.length; i++ ) {
         textSquares[i] = this.makeTextSquare(x, y, this.numbers[i]);
-        stage.addChild(textSquares[i]);
+        this.stage.addChild(textSquares[i]);
         x += 50;
     }
 
     // textSquares[0].addEventListener('click', function(event) {console.log("type: ", event.target, ", stageX: ", event.stageX); })
 
-    stage.update(); 
+    this.stage.update(); 
     
   }
 
   handleCanvasClick = (event) => {
     // console.log("hellow");
-    console.log("type: ", event.target);
-    console.log("x-location: ", event.target.graphics.command.x);
-    console.log("y-location: ", event.target.graphics.command.y);
-    console.log("inner text: ", event.target.parent.children[1].text);
+    // console.log("type: ", event.target);
+    // console.log("x-location: ", event.target.graphics.command.x);
+    // console.log("y-location: ", event.target.graphics.command.y);
+    // console.log("inner text: ", event.target.parent.children[1].text);
 
     // increment canvasClicks since the user clicked on a canvas element
     this.canvasClicks++;
-    console.log("Increased targetClicks to: ", this.canvasClicks);
+    // console.log("Increased targetClicks to: ", this.canvasClicks);
 
     // Store the clicked number and the element location
     this.operands[this.canvasClicks-1] = event.target.parent.children[1].text;
-    this.operandLocations[this.canvasClicks-1] = event.target.graphics.command;
+    // this.operandLocations[this.canvasClicks-1] = event.target.graphics.command;
+    this.operandLocationX[this.canvasClicks-1] = event.target.parent.getTransformedBounds().x-6;
+    // console.log("Assigning this x location: ", event.target.parent.getTransformedBounds().x-6);
+    this.operandContainers[this.canvasClicks-1] = event.target.parent;
 
-    // If there have been 2 canvasClicks, run the operation and reset canvasClicks
-    if(this.canvasClicks===2) {
-
-        // run the operation
-        this.runOperation();
-        // resent canvasClicks to 0
-        this.canvasClicks = 0;
-        // todo store both operands on a stack of what was switched
+    // move it down a little
+    this.operandContainers[this.canvasClicks-1].y+=10;
+    this.stage.update();
 
 
-    } else {
-        // todo put a highlight around the first clicked element?
+    // first canvasClick just move the element down a little
+    if (this.canvasClicks===1) {
+        this.operandContainers[this.canvasClicks-1].y+=10;
+        this.stage.update();
+    } else { // second canvasClick run operation if clicked on 2 different elements
+        this.canvasClicks = 0; // reset canvasClicks to 0
+        if (this.operandContainers[0] === this.operandContainers[1]) {
+            // if the clicked on the same element twice move it back to where it was
+            this.operandContainers[0].y=0;
+            this.stage.update();
+        } else {
+            this.operandContainers[1].y+=10;
+            this.stage.update();
+
+            this.runOperation();
+            // todo store both operands on a stack of what was switched
+        }
     }
   }
 
   runOperation(){
-      console.log("Running the operation");
+      // console.log("Running the operation");
       switch (this.operation) {
-          case 'swap':
+          case 'Swap':
             this.swap();
             break;
           case 'split':
@@ -126,13 +174,11 @@ class Lesson extends React.Component {
           default:
               break;
       }
-
   }
 
-  swap(){
-    console.log("running a swap operation with operands ", this.operands);
-    // monkey - need to program the animation to move these
-  }
+
+  
+
 
   makeTextSquare(x, y, num){
     let textSquare = new Container();
