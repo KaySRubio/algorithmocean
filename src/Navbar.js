4fa5bd644e1bc8ced7859d8da739f25acd5224af
Kay Rubio <krubio@cainc.com>
2022-05-18 15:06:15 -0400

    Updating navbar for phone screens

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,6 +5,8 @@ import wave2 from './img/wave2.png';
 import { setTheme } from './utils/themes';
 import toggle1 from './img/toggle1.png';
 import toggle2 from './img/toggle2.png';
+import hamburgDark from './img/hamburgDark.png';
+import hamburgLight from './img/hamburgLight.png';
 // import PropTypes from "prop-types";
 // import { withRouter } from "react-router";
 
@@ -47,31 +49,57 @@ class Navbar extends React.Component {
     console.log("updated theme to: ", this.state.theme);
   }
 
+  openClosePhoneMenu = () => {
+    const phoneDropdown = document.getElementById('phoneDropdown');
+    if ( phoneDropdown.getAttribute('class') === 'notHidden' ) {
+      phoneDropdown.setAttribute("class", "hidden");
+    }
+    else phoneDropdown.setAttribute("class", "notHidden");
+  }
 
   render(){
     return (
       <div className="navbar">
+        <div className='phone-only'>
+          { this.state.theme==='theme-light' && <button id="menuButton" onClick={this.openClosePhoneMenu}><img src={hamburgDark} className="hamburg" alt="A menu button"/></button> }
+          { this.state.theme==='theme-dark' && <button id="menuButton" onClick={this.openClosePhoneMenu}><img src={hamburgLight} className="hamburg" alt="A menu button"/></button> }
+          <div id='phoneDropdown' className='hidden' >
+            <Link className="link navbarlink phoneDropdownLink" to="/">Home</Link> <br />
+            <Link className="link navbarlink phoneDropdownLink" to="/contact">Contact</Link> <br />
+            <Link className="link navbarlink phoneDropdownLink" to="/login">Login</Link> <br />
+            <Link className="link navbarlink phoneDropdownLink" to="/createaccount">Create Account</Link> <br />
+            <Link className="link navbarlink phoneDropdownLink" to="/demo-lesson/bubble">Demo Bubble Sort</Link> <br /> 
+            <Link className="link navbarlink phoneDropdownLink" to="/demo-lesson/insertion">Demo Insertion Sort</Link> <br />
+            <Link className="link navbarlink phoneDropdownLink" to="/demo-lesson/selection">Demo Selection Sort</Link> <br />
+          </div>
+        </div>
+
         <div id="logo">
           <p><strong>AlgorithmOcean</strong></p>
           { this.state.theme === 'theme-light' && <img src={wave1} className="logowave" title="dark blue wave" alt="Algorithm Ocean logo that includes a blue ocean wave"/> }
           { this.state.theme === 'theme-dark' && <img src={wave2} className="logowave" title="light blue wave" alt="Algorithm Ocean logo that includes a blue ocean wave"/> }
         </div>
+        <div id='menu'>
 
-        <Link className="link navbarlink" to="/">Home</Link>
-        <Link className="link navbarlink" to="/contact">Contact</Link>
-        <Link className="link navbarlink" to="/login">Login</Link>
-        <Link className="link navbarlink" to="/createaccount">Create Account</Link>
-        
-        <div className='dropdown'>
-          <p>Try one!</p>
-          <div className='dropdownContent'>
-              <Link className="link" to="/demo-lesson/bubble">Bubble Sort</Link>
-              <br />
-              <Link className="link" to="/demo-lesson/insertion">Insertion Sort</Link>
-              <br />
-              <Link className="link" to="/demo-lesson/selection">Selection Sort</Link>
+          <Link className="link navbarlink" to="/">Home</Link>
+          <Link className="link navbarlink" to="/contact">Contact</Link>
+          <Link className="link navbarlink" to="/login">Login</Link>
+          <Link className="link navbarlink" to="/createaccount">Create Account</Link>
+          
+          <div className='dropdown'>
+            <p>Try one!</p>
+            <div className='dropdownContent'>
+                <Link className="link" to="/demo-lesson/bubble">Bubble Sort</Link>
+                <br />
+                <Link className="link" to="/demo-lesson/insertion">Insertion Sort</Link>
+                <br />
+                <Link className="link" to="/demo-lesson/selection">Selection Sort</Link>
+            </div>
           </div>
         </div>
+
+
+
         <button id="toggleButton" onClick={this.handleOnClick}>
           { this.state.theme==='theme-light' && <img src={toggle2} className="toggle" alt="A toggle switch that changes the background color from light to dark"/> }
           { this.state.theme==='theme-dark' && <img src={toggle1} className="toggle" alt="A toggle switch that changes the background color from light to dark"/> }
