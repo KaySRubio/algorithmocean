4a9effcca5b7b14918c1513e3b2a9fd985936ed5
DontFreakOut <dontfreakout@DontFreakOuts-MacBook-Pro.local>
2022-06-14 14:42:39 -0400

    Updated dashboard for production build:

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -13,7 +13,10 @@ class Dashboard extends React.Component {
   componentDidMount() {
     const first_name = localStorage.getItem('first_name');
     console.log('first_name', first_name);
-    if (first_name === null || first_name === undefined || first_name === 'undefined') window.location.replace('http://localhost:3000/login');
+    if (first_name === null || first_name === undefined || first_name === 'undefined') {
+      // window.location.replace('http://localhost:3000/login'); // Development
+      window.location.replace('https://stormy-sierra-07970.herokuapp.com/login'); // Production
+    }
     this.setState({ first_name: first_name });
   }
   
